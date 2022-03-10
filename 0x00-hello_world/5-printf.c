0913d93b3b95e627c16cb94e549996c73e3c9d29
George Kibe <kiberenge94@gmail.com>
2022-03-10 23:13:18 +0300

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,19 +1,10 @@
 #include <stdio.h>
 
-/*
-Write a C program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line.
-
-Use the function printf
-You are not allowed to use the function puts
-Your program should return 0
-Your program should compile without warning when using the -Wall gcc option
-julien@ubuntu:~/c/0x00$ gcc -Wall -Werror -Wextra -pedantic -std=gnu89 5-printf.c
-julien@ubuntu:~/c/0x00$ ./a.out
-with proper grammar, but the outcome is a piece of art,
-julien@ubuntu:~/c/0x00$ echo $?
-0
-julien@ubuntu:~/c/0x00$
-*/
+/**
+ * main - prints the phrase with proper grammar,
+ * but the outcome is a piece of art,
+ * Return: 0 if exited properly, non-zero otherwise
+ */
 
 int main(void)
 {
