cc4dba9de97172853879540baa3660132ae0f1e8
George Kibe <kiberenge94@gmail.com>
2022-03-15 21:59:29 +0300

    task 0enforced betty style

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,9 @@
 #include "main.h"
-
-int main ()
+/**
+ * main -  prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
 	_putchar('_');
 	_putchar('p');
@@ -11,5 +14,5 @@ int main ()
 	_putchar('a');
 	_putchar('r');
     _putchar('\n');
-	return 0;
+	return (0);
 }
