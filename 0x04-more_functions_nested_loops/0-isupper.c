95a156d9b5511f6a9054b5402bcafaa7e7060c28
George Kibe <kiberenge94@gmail.com>
2022-03-17 15:48:11 +0300

    Betty task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -8,15 +8,5 @@
  */
 int _isupper(int c)
 {
-	/*int b = 0;
-	if (c >= 97 && c <= 122)
-	{
-		b = 0;
-	}
-	else if (c >= 65 && c <= 90)
-	{
-		b = 1;
-	}
-	return (b);*/
 	return (c >= 'A' && c <= 'Z');
 }
