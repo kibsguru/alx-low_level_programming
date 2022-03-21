ba4d2feb6511086b633e00abe86a02b0a210edfe
George Kibe <kiberenge94@gmail.com>
2022-03-21 21:50:25 +0300

    Task 1 betty update

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,12 +1,15 @@
 #include "main.h"
 
 /**
- * function swap_int swaps @a and @b using an extra variable @c
+ * function swap_int swaps 
+ * @a points to the first integer and 
+ * @b point to the second variable
  */
 
 void swap_int(int *a, int *b)
 {
-	int c ;
+	int c;
+	
 	c = *a;
 	*a = *b;
 	*b = c;
