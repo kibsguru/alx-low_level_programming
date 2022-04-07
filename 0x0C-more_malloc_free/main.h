a558e0a8c697b5507fd0badad62127e625b92e78
George Kibe <kiberenge94@gmail.com>
2022-04-08 01:08:12 +0300

    task advanced

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -3,7 +3,11 @@
 
 #include <stdlib.h>
 
-int _puts(char c);
+int _putchar(char c);
+void errors(void);
+int _strlen(char *s);
+int is_digit(char *s);
+
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
@@ -12,6 +16,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _putchar(char c);
 int _atoi(const char *s);
 void print_int(unsigned long int n);
-
+void _puts(char *str);
 
 #endif
